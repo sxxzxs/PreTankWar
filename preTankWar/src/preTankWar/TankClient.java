fdadcdd78d30cd539624f0e93913c8c6ccf4d3e8
pangxiaoshuai <529971049@qq.com>
2019-05-25 12:12:11 +0800

    添加了键盘监听，按不同方向的键实现对应方向的移动

--- preTankWar/src/preTankWar/TankClient.java
+++ preTankWar/src/preTankWar/TankClient.java
@@ -12,11 +12,15 @@ public class TankClient extends Frame{
 		
 	public void launch() {
 		setLocation(300, 100);	//窗口位置
-		setSize(GAME_WIDTH, GAME_HEIGHT);	 //窗口大小
-		setVisible(true);	//设置可见
+		setSize(GAME_WIDTH, GAME_HEIGHT);	 //窗口大小		
 		setResizable(false); 	//窗口大小可变否
 		setTitle("TankWar");	//窗口标题栏
 		setBackground(Color.BLACK);
+		
+		//添加事件监听
+		addKeyListener(new KeyMonitor());
+		
+		setVisible(true);	//设置可见
 		
 		//设置窗口监听，点拔插可关闭窗口
 		addWindowListener(new WindowAdapter() {
@@ -38,8 +42,6 @@ public class TankClient extends Frame{
 		g.setColor(Color.RED);
 		g.fillOval(x, y, 50, 50);	//画出一个圆
 		g.setColor(c);
-		
-		y += 5;
 	}
 	
 	/*解决双缓冲,没必要深究，截获update,首先把画出来的东西（先画在内存的图片中，
@@ -65,7 +67,7 @@ public class TankClient extends Frame{
 	}
 	
 	//定义线程继承Runnable接口
-	class PaintThread implements Runnable {
+	private class PaintThread implements Runnable {
 		public void run() {
 			while(true) {
 				//调用repaint方法后先调用update在调用paint
@@ -77,6 +79,29 @@ public class TankClient extends Frame{
 				}
 			}
 		}
+	}
+	
+	//定义监听器类实现键盘控制
+	private class KeyMonitor extends KeyAdapter{
+		@Override
+		public void keyPressed(KeyEvent e) {
+			int key = e.getKeyCode();	//获得所按键的虚拟键码
+			switch(key) {
+			case KeyEvent.VK_LEFT:
+				x -= 10;
+				break;
+			case KeyEvent.VK_UP:
+				y -= 10;
+				break;
+			case KeyEvent.VK_RIGHT:
+				x += 10;
+				break;
+			case KeyEvent.VK_DOWN:
+				y += 10;
+				break;
+			}
+		}
+		
 	}
 
 }
