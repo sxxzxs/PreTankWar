3f65a127aade7138fe1f76f1ed0d2172508c0a17
pangxiaoshuai <529971049@qq.com>
2019-05-25 10:41:03 +0800

    添加标题，设置窗口大熊不可变，设置窗口监听，可关闭窗口。重写paint方法，在窗口里画出一个圆

--- preTankWar/src/preTankWar/TankClient.java
+++ preTankWar/src/preTankWar/TankClient.java
@@ -1,14 +1,34 @@
 package preTankWar;
 
 import java.awt.*;
+import java.awt.event.*;
 
 
 public class TankClient extends Frame{
 		
 	public void launch() {
-		setLocation(300, 100);
-		setSize(800, 600);
-		setVisible(true);
+		setLocation(300, 100);	//窗口位置
+		setSize(800, 600);	 //窗口大小
+		setVisible(true);	//设置可见
+		setResizable(false); 	//窗口大小可变否
+		setTitle("TankWar");	//窗口标题栏
+		//设置窗口监听，点拔插可关闭窗口
+		addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			}			
+		});
+		
+	}
+	
+	//paint方法，窗口重画时候自动调用
+	@Override
+	public void paint(Graphics g) {
+		Color c = g.getColor();
+		g.setColor(Color.BLACK);
+		g.fillOval(30, 50, 50, 50);	//画出一个圆
+		g.setColor(c);
 	}
 	
 	
