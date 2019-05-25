0f2d56e7cc2e712ebc46137a60fd66cc13ff1da9
pangxiaoshuai <529971049@qq.com>
2019-05-25 11:07:24 +0800

    定义线程继承Runnable接口，每隔一段时间调用repaint方法，repaint方法又调用paint方法，实现tank的移动

--- preTankWar/src/preTankWar/TankClient.java
+++ preTankWar/src/preTankWar/TankClient.java
@@ -5,6 +5,7 @@ import java.awt.event.*;
 
 
 public class TankClient extends Frame{
+	int x = 30, y = 50;
 		
 	public void launch() {
 		setLocation(300, 100);	//窗口位置
@@ -12,6 +13,8 @@ public class TankClient extends Frame{
 		setVisible(true);	//设置可见
 		setResizable(false); 	//窗口大小可变否
 		setTitle("TankWar");	//窗口标题栏
+		setBackground(Color.GREEN);
+		
 		//设置窗口监听，点拔插可关闭窗口
 		addWindowListener(new WindowAdapter() {
 			@Override
@@ -20,15 +23,20 @@ public class TankClient extends Frame{
 			}			
 		});
 		
+		//启动线程
+		new Thread(new PaintThread()).start();
+		
 	}
 	
 	//paint方法，窗口重画时候自动调用
 	@Override
 	public void paint(Graphics g) {
 		Color c = g.getColor();
-		g.setColor(Color.BLACK);
-		g.fillOval(30, 50, 50, 50);	//画出一个圆
+		g.setColor(Color.RED);
+		g.fillOval(x, y, 50, 50);	//画出一个圆
 		g.setColor(c);
+		
+		y += 5;
 	}
 	
 	
@@ -36,6 +44,20 @@ public class TankClient extends Frame{
 		
 		TankClient tc = new TankClient();
 		tc.launch();
+	}
+	
+	//定义线程继承Runnable接口
+	class PaintThread implements Runnable {
+		public void run() {
+			while(true) {
+				repaint();
+				try {
+					Thread.sleep(50);
+				} catch (InterruptedException e) {				
+					e.printStackTrace();
+				}
+			}
+		}
 	}
 
 }
