534ca95ddd15438578d967049fbbcfdf516b90f5
pangxiaoshuai <529971049@qq.com>
2019-05-25 11:39:35 +0800

    添加update方法解决双缓冲，没必要深究，截获update,首先把画出来的东西（先画在内存的图片中， 	图片大小和游戏画面一致，然后把内存中图片一次性画到屏幕上（把内存的内容复制到显存）。repaint调用后先调用update方法,之后调用paint方法

--- preTankWar/src/preTankWar/TankClient.java
+++ preTankWar/src/preTankWar/TankClient.java
@@ -6,6 +6,7 @@ import java.awt.event.*;
 
 public class TankClient extends Frame{
 	int x = 30, y = 50;
+	Image offScreenImage = null;
 		
 	public void launch() {
 		setLocation(300, 100);	//窗口位置
@@ -13,7 +14,7 @@ public class TankClient extends Frame{
 		setVisible(true);	//设置可见
 		setResizable(false); 	//窗口大小可变否
 		setTitle("TankWar");	//窗口标题栏
-		setBackground(Color.GREEN);
+		setBackground(Color.BLACK);
 		
 		//设置窗口监听，点拔插可关闭窗口
 		addWindowListener(new WindowAdapter() {
@@ -39,6 +40,21 @@ public class TankClient extends Frame{
 		y += 5;
 	}
 	
+	/*解决双缓冲,没必要深究，截获update,首先把画出来的东西（先画在内存的图片中，
+	图片大小和游戏画面一致，然后把内存中图片一次性画到屏幕上（把内存的内容复制到显存）*/
+	@Override
+	public void update(Graphics g) {
+		if(offScreenImage == null) {
+			offScreenImage = this.createImage(800, 600);
+		}
+		Graphics gOffScreen = offScreenImage.getGraphics();	//拿到图片的画笔
+		Color c = gOffScreen.getColor();
+		gOffScreen.setColor(Color.BLACK);
+		gOffScreen.fillRect(0, 0, 800, 600);
+		gOffScreen.setColor(c);
+		paint(gOffScreen);	
+		g.drawImage(offScreenImage, 0, 0, null);
+	}
 	
 	public static void main(String[] args) {
 		
@@ -50,6 +66,7 @@ public class TankClient extends Frame{
 	class PaintThread implements Runnable {
 		public void run() {
 			while(true) {
+				//调用repaint方法后先调用update在调用paint
 				repaint();
 				try {
 					Thread.sleep(50);
