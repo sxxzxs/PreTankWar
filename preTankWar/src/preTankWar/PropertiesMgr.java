f79fa2d2d541b3e9c6fbb12653b72617644195ec
pangxiaoshuai <529971049@qq.com>
2019-05-28 13:17:03 +0800

    备注了些许PropertiesMgr里的信息

--- preTankWar/src/preTankWar/PropertiesMgr.java
+++ preTankWar/src/preTankWar/PropertiesMgr.java
@@ -8,7 +8,7 @@ public class PropertiesMgr {
 	static {
 
 		try {
-			//加载配置文件
+			//load(InputStream inStream)从输入字节流中读取属性列表(键和元素对),返回值是void。
 			props.load(PropertiesMgr.class.getClassLoader().getResourceAsStream("config/tank.properties"));
 		} catch (IOException e1) {			
 			e1.printStackTrace();
@@ -19,6 +19,6 @@ public class PropertiesMgr {
 	
 	public static String getProperty(String key) {
 		
-		return props.getProperty(key);	//返回key所对应的映射值
+		return props.getProperty(key);	//返回key所对应的元素对(有点像Map里的映射)
 	}
 }
